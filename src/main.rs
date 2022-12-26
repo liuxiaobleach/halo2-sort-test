a4c62dd8585825f502794db6dd8ef08e4eb6cbd5
liuxiaobleach <1241368737@qq.com>
2022-12-26 21:59:05 +0800

    add public input constraint

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,11 @@ const MAX_VAL: u64 = 100;
 #[derive(Clone, Debug)]
 struct SortConfig {
     advices: [Column<Advice>; ELEMENT_LEN],
-    instance: Column<Instance>,
+    instances: [Column<Instance>; ELEMENT_LEN],
     s_is_less_than: Selector,
     s_diff: Selector,
+    s_one_eq: Selector,
+    s_sum_eq: Selector,
 }
 
 #[derive(Clone, Debug)]
@@ -42,16 +44,20 @@ impl<F: FieldExt> SortChip<F> {
     fn configure(
         meta: &mut ConstraintSystem<F>,
         advices: [Column<Advice>; ELEMENT_LEN],
-        instance: Column<Instance>,
+        instances: [Column<Instance>; ELEMENT_LEN],
     ) -> SortConfig {
-        meta.enable_equality(instance);
         for col in advices {
+            meta.enable_equality(col);
+        }
+        for col in instances {
             meta.enable_equality(col);
         }
         let s_is_less_than = meta.selector();
         let s_diff = meta.selector();
+        let s_one_eq = meta.selector();
+        let s_sum_eq = meta.selector();
 
-        meta.create_gate("bool check", |meta| {
+        meta.create_gate("bool check for less than", |meta| {
             let selector = meta.query_selector(s_is_less_than);
             let advice = meta.query_advice(advices[0], Rotation::cur());
             Constraints::with_selector(selector, Some(range_check(advice, MAX_VAL)))
@@ -66,15 +72,54 @@ impl<F: FieldExt> SortChip<F> {
             vec![s_diff * (a + max_range - b - diff)]
         });
 
-        SortConfig {advices, instance, s_is_less_than, s_diff}
+        meta.create_gate("one eq", |meta| {
+            let s_one_eq = meta.query_selector(s_one_eq);
+            let max_range = Expression::Constant(F::from(MAX_VAL));
+            let a = meta.query_advice(advices[0], Rotation::cur());
+
+            let diff_a_1 = meta.query_advice(advices[0], Rotation::next());
+            let diff_a_2 = meta.query_advice(advices[1], Rotation::next());
+            let diff_a_3 = meta.query_advice(advices[2], Rotation::next());
+
+            let i_a = meta.query_advice(advices[0], Rotation(2));
+            let i_b = meta.query_advice(advices[1], Rotation(2));
+            let i_c = meta.query_advice(advices[2], Rotation(2));
+
+            vec![s_one_eq.clone() * (a.clone() + max_range.clone() - i_a.clone() - diff_a_1.clone()),
+                 s_one_eq.clone() * (a.clone() + max_range.clone() - i_b.clone() - diff_a_2.clone()),
+                 s_one_eq.clone() * (a.clone() + max_range.clone() - i_c.clone() - diff_a_3.clone()),
+                 s_one_eq.clone() * (diff_a_1.clone() - max_range.clone()) * (diff_a_2.clone() - max_range.clone()) * (diff_a_3.clone() - max_range.clone())
+            ]
+        });
+
+        meta.create_gate("eq pub input", |meta| {
+            let sum_eq = meta.query_selector(s_sum_eq);
+            let a = meta.query_advice(advices[0], Rotation::cur());
+            let b = meta.query_advice(advices[1], Rotation::cur());
+            let c = meta.query_advice(advices[2], Rotation::cur());
+
+            let i_a = meta.query_advice(advices[0], Rotation::next());
+            let i_b = meta.query_advice(advices[1], Rotation::next());
+            let i_c = meta.query_advice(advices[2], Rotation::next());
+
+            vec![sum_eq * (a + b + c - i_a - i_b - i_c)]
+        });
+
+        SortConfig {advices, instances, s_is_less_than, s_diff, s_one_eq, s_sum_eq}
     }
 
     fn assign_all(&self, mut layouter: impl Layouter<F>, val: [Value<F>; ELEMENT_LEN]) -> Result<[Number<F>; ELEMENT_LEN], Error> {
         let config = self.config();
-        layouter.assign_region(|| "copy", |mut region| {
+
+        layouter.assign_region(|| "load private", |mut region| {
+            config.s_sum_eq.enable(&mut region, 0)?;
             let a = region.assign_advice(|| "a", config.advices[0], 0, || val[0]).map(Number).unwrap();
             let b = region.assign_advice(|| "b", config.advices[1], 0, || val[1]).map(Number).unwrap();
             let c = region.assign_advice(|| "c", config.advices[2], 0, || val[2]).map(Number).unwrap();
+
+            region.assign_advice_from_instance(|| "a", config.instances[0], 0, config.advices[0], 1)?;
+            region.assign_advice_from_instance(|| "a", config.instances[1], 0, config.advices[1], 1)?;
+            region.assign_advice_from_instance(|| "a", config.instances[2], 0, config.advices[2], 1)?;
             Ok([a,b,c])
         })
     }
@@ -95,6 +140,7 @@ impl<F: FieldExt> SortChip<F> {
                 b.0.copy_advice(|| "rhs b", &mut region, config.advices[1], 0).map(Number).unwrap();
                 let diff = a.0.value().copied() + max - b.0.value() ;
                 let diff = region.assign_advice(|| "diff", config.advices[2], 0, || diff).map(Number).unwrap();
+
                 Ok(diff)
             })
     }
@@ -112,6 +158,35 @@ impl<F: FieldExt> SortChip<F> {
                 Ok(())
             },
         )
+    }
+
+    pub fn assign_instance_diff(
+        &self,
+        mut layouter: impl Layouter<F>,
+        a: Number<F>,
+        instance: [Value<F>; ELEMENT_LEN],
+        max: Value<F>,
+    ) -> Result<(), Error> {
+        let config = self.config();
+        layouter.assign_region(
+            || "assign eq diff",
+            |mut region| {
+                config.s_one_eq.enable(&mut region, 0)?;
+                a.0.copy_advice(|| "one val", &mut region, config.advices[0], 0).map(Number).unwrap();
+
+                let diff_1 = a.0.value().copied() + max - instance[0];
+                let diff_2 = a.0.value().copied() + max - instance[1];
+                let diff_3 = a.0.value().copied() + max - instance[2];
+                region.assign_advice(|| "one instance diff", config.advices[0], 1, || diff_1).map(Number).unwrap();
+                region.assign_advice(|| "one instance diff", config.advices[1], 1, || diff_2).map(Number).unwrap();
+                region.assign_advice(|| "one instance diff", config.advices[2], 1, || diff_3).map(Number).unwrap();
+
+                region.assign_advice_from_instance(|| "a", config.instances[0], 0, config.advices[0], 2)?;
+                region.assign_advice_from_instance(|| "a", config.instances[1], 0, config.advices[1], 2)?;
+                region.assign_advice_from_instance(|| "a", config.instances[2], 0, config.advices[2], 2)?;
+
+                Ok(())
+            })
     }
 
 }
@@ -133,6 +208,7 @@ impl<F: FieldExt> Chip<F> for SortChip<F> {
 struct SortCircuit<F: FieldExt> {
     list: [F; ELEMENT_LEN],
     max: Value<F>,
+    instances: [Value<F>; ELEMENT_LEN],
 }
 
 impl<F: FieldExt> Circuit<F> for SortCircuit<F> {
@@ -145,9 +221,8 @@ impl<F: FieldExt> Circuit<F> for SortCircuit<F> {
 
     fn configure(meta: &mut ConstraintSystem<F>) -> Self::Config {
         let advices = [meta.advice_column(), meta.advice_column(), meta.advice_column()];
-        let instance = meta.instance_column();
-        // TODO use instance later
-        SortChip::configure(meta, advices, instance)
+        let instances = [meta.instance_column(), meta.instance_column(), meta.instance_column()];
+        SortChip::configure(meta, advices, instances)
     }
 
     fn synthesize(&self, config: Self::Config, mut layouter: impl Layouter<F>) -> Result<(), Error> {
@@ -171,14 +246,15 @@ impl<F: FieldExt> Circuit<F> for SortCircuit<F> {
         let new_arr = sort_chip.assign_all(layouter.namespace(|| "one"),
                                            [Value::known(raw[0]), Value::known(raw[1]), Value::known(raw[2])])?;
 
-        let diff = sort_chip.assign_diff(layouter.namespace(|| "load diff a,b"),
-                                         new_arr[0].clone(), new_arr[1].clone(), self.max)?;
+        //let diff1inv = diff1.0.value().map(|v| v.invert().unwrap_or(F::zero()));
+        // private input is a sorted array
+        let diff1 = sort_chip.assign_diff(layouter.namespace(|| "load diff a,b"), new_arr[0].clone(), new_arr[1].clone(), self.max)?;
+        sort_chip.check_less_than(layouter.namespace(|| "check diff in range"), diff1)?;
+        let diff2 = sort_chip.assign_diff(layouter.namespace(|| "load diff b c"), new_arr[1].clone(), new_arr[2].clone(), self.max)?;
+        sort_chip.check_less_than(layouter.namespace(|| "check diff in range"), diff2)?;
 
-        sort_chip.check_less_than(layouter.namespace(|| "check diff in range"), diff)?;
-
-        let diff = sort_chip.assign_diff(layouter.namespace(|| "load diff b c"),
-                                         new_arr[1].clone(), new_arr[2].clone(), self.max)?;
-        sort_chip.check_less_than(layouter.namespace(|| "check diff in range"), diff)?;
+        // private input is equal to public input
+        sort_chip.assign_instance_diff(layouter.namespace(|| "diff of a with instances"), new_arr[0].clone(), self.instances, self.max)?;
 
         Ok(())
     }
@@ -187,6 +263,7 @@ impl<F: FieldExt> Circuit<F> for SortCircuit<F> {
 fn main() {
     use halo2_proofs::{dev::MockProver};
     let k = 4;
+    // use a,b,c as public input
     let a = Fp::from(3);
     let b = Fp::from(2);
     let c = Fp::from(1);
@@ -195,10 +272,11 @@ fn main() {
     let circuit = SortCircuit {
         list: [a,b,c],
         max: Value::known(max),
+        instances: [Value::known(a), Value::known(b), Value::known(c)],
     };
 
     // while instance may not used, init still need it. Or err "invalidInstances will be returned."
-    let prover = MockProver::run(k, &circuit, vec![vec![]]).unwrap();
+    let prover = MockProver::run(k, &circuit, vec![vec![a], vec![b], vec![c]]).unwrap();
     assert_eq!(prover.verify(), Ok(()));
 
     // optional, draw layout picture
